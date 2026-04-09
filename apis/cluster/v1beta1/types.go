3ec8c36603ee614560907a8e275e8738ba910f7b
Yasmany Cubela Medina <bitgandtter@Yasmanys-Mac-mini.local>
2026-04-09 11:40:32 -0500

    fix: ProviderConfigUsageList GetItems return type + add image Makefile
    - Fix GetItems() to return []resource.ProviderConfigUsage (interface) instead
      of []ProviderConfigUsage (concrete), fixing family provider CrashLoopBackOff
    - Add cluster/images Makefile for monolith build support

--- apis/cluster/v1beta1/types.go
+++ apis/cluster/v1beta1/types.go
@@ -8,6 +8,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	xpv1 "github.com/crossplane/crossplane-runtime/v2/apis/common/v1"
+	"github.com/crossplane/crossplane-runtime/v2/pkg/resource"
 )
 
 // A ProviderCredentials defines credentials for a provider.
@@ -114,6 +115,10 @@ type ProviderConfigUsageList struct {
 }
 
 // GetItems of this ProviderConfigUsageList.
-func (p *ProviderConfigUsageList) GetItems() []ProviderConfigUsage {
-	return p.Items
+func (p *ProviderConfigUsageList) GetItems() []resource.ProviderConfigUsage {
+	items := make([]resource.ProviderConfigUsage, len(p.Items))
+	for i := range p.Items {
+		items[i] = &p.Items[i]
+	}
+	return items
 }
